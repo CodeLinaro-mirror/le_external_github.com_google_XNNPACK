f9d7f6481e18d3d1593dcb72d445a587c657bdfb
Dillon Sharlet <dsharlet@google.com>
2024-08-03 17:31:14 -0700

    Move constants from microparams to kernels for sigmoid
    PiperOrigin-RevId: 659176115

--- src/xnnpack/common.h
+++ src/xnnpack/common.h
@@ -381,14 +381,16 @@
   #define XNN_MULTIPASS_EXTRA_BYTES 16
 #endif
 
-#if XNN_ARCH_ARM || XNN_ARCH_X86
+#if XNN_ARCH_ARM || XNN_ARCH_X86 || XNN_ARCH_WASM || XNN_ARCH_WASMSIMD || XNN_ARCH_WASMRELAXEDSIMD
   // These architectures are slow to broadcast, the compiler tries to move them
   // into loops, and when it runs out of registers, it will redundantly perform
   // the broadcast. Marking them volatile prevents these from being moved into
   // loops, and they spill as broadcasted vectors instead.
   #define XNN_FORCE_STACK volatile
+  #define XNN_FORCE_REALIZATION(x) __asm volatile(""::"m"(x));
 #else
   #define XNN_FORCE_STACK
+  #define XNN_FORCE_REALIZATION(x)
 #endif
 
 #define XNN_LOG2_SIZEOF_INT8_T   0  // log2(sizeof(int8_t))
